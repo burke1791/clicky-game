482b1e2425d54cbda147b1110db89627aee731bd
Chris Burke <burkcules@Chriss-iMac.local>
2019-07-27 03:44:50 -0500

    initialized with CRA and downloaded images for the game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # clicky-game
 Simple browser memory game
+
+logo images were cropped from: https://www.sporcle.com/games/JackDots/big-10-logo-click#
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component () {
+  
+  
+  rener() {
+    return (
+      <div className="App">
+        <Header />
+        <Jumbotron />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
